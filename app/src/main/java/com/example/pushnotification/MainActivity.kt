5aaf6c471df35ea837835adf8b02daae5a16d594
Irakli Dekanosidze <88934243+iraklideka94@users.noreply.github.com>
2022-10-06 22:14:21 +0400

    Add splashScreen

--- app/src/main/java/com/example/pushnotification/MainActivity.kt
+++ app/src/main/java/com/example/pushnotification/MainActivity.kt
@@ -2,10 +2,15 @@ package com.example.pushnotification
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen()
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+
 }
